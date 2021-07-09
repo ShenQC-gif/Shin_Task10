48825e7e7e42650a1d18968c6b97d337deb5edf6
akio0911 <akio0911@gmail.com>
2021-07-09 18:03:08 +0900

    feedback

--- Task10/TableViewController.swift
+++ Task10/TableViewController.swift
@@ -8,35 +8,34 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    private var prefectureRepository = PrefectureRepostiroy()
+    private let prefectureRepository = PrefectureRepostiroy()
+
+    private var prefectureNames: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prefectureRepository.fetch(completion: { [weak self] in
+            self?.prefectureNames = $0
+            self?.tableView.reloadData()
+        })
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        prefectureRepository.dataArray.count
+        prefectureNames.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        cell.textLabel?.text = prefectureRepository.dataArray[indexPath.row]
+        cell.textLabel?.text = prefectureNames[indexPath.row]
 
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
 
-        switch indexPath.row % 3 {
-        case 0:
-            cell.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
-        case 1:
-            cell.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-        case 2:
-            cell.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
-        default:
-            break
-        }
+        let colors: [UIColor] = [#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)]
+        cell.backgroundColor = colors[indexPath.row % colors.count]
 
         return cell
     }
